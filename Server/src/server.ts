f2dcd622f0c74181d82df76947f482a366a354d7
ayushpaharia <ayush.paharia.18@gmail.com>
2021-07-03 15:50:34 +0530

    jwt-session-auth server.ts setup, logger and mongo connection added

---
+++ Server/src/server.ts
@@ -0,0 +1,25 @@
+console.clear();
+
+import express from "express";
+import config from "config";
+import connectToDatabase from "./db/connect";
+// import "./routes.ts";
+import log from "./logger";
+// import routes from "./routes";
+// import { deserializeUser } from "./middleware";
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+
+const app = express();
+// app.use(deserializeUser);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+  log.info(`Server listing at http://${host}:${port}/`);
+
+  //   connect();
+  //   routes(app);
+});
